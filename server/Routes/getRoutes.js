550192ae784ebd72611bb87c5f4be96dad02e277
lazarevkristijan <lazarevkristijan@gmail.com>
2024-01-15 14:12:39 +0100

    add routes dir

---
+++ server/Routes/getRoutes.js
@@ -0,0 +1,13 @@
+import sql from "./db.js"
+
+export const getUsers = async (req, res) => {
+  try {
+    const users = await sql`
+        SELECT * FROM users`
+
+    return res.json(users)
+  } catch (error) {
+    console.error("Error is: ", error)
+    return res.status(500).json({ error: "Error when getting all users" })
+  }
+}
--- server/app.js
+++ server/app.js
@@ -1,5 +1,5 @@
 import express from "express"
-import sql from "./db.js"
+import { getUsers } from "./Routes/getRoutes.js"
 
 const app = express()
 const port = process.env.DB_PORT
@@ -9,17 +9,7 @@ const server = app.listen(port, () =>
 
 app.get("/", (req, res) => res.send("DB ROOT"))
 
-app.get("/users", async (req, res) => {
-  try {
-    const users = await sql`
-    SELECT * FROM users`
+app.get("/users", getUsers)
 
-    return res.json(users)
-  } catch (error) {
-    console.error("Error is: ", error)
-    return res.status(500).json({ error: "Error when getting all users" })
-  }
-})
-
-server.keepAliveTimeout = 120 * 1000
+server.keepAliveTimeout = 6000 * 1000
 server.headersTimeout = 120 * 1000
