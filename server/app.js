5a395a1879b8b49ea504f1f5ecb73293a85430be
lazarevkristijan <lazarevkristijan@gmail.com>
2024-01-15 14:07:07 +0100

    add example get route

--- server/app.js
+++ server/app.js
@@ -1,13 +1,25 @@
 import express from "express"
+import sql from "./db.js"
 
 const app = express()
-const port = process.env.PORT || 3001
-
-app.get("/", (req, res) => res.send("DB ROOT"))
-
-server.keepAliveTimeout = 120 * 1000
-server.headersTimeout = 120 * 1000
-
+const port = process.env.DB_PORT
 const server = app.listen(port, () =>
   console.log(`Alias backend listening on port ${port}!`)
 )
+
+app.get("/", (req, res) => res.send("DB ROOT"))
+
+app.get("/users", async (req, res) => {
+  try {
+    const users = await sql`
+    SELECT * FROM users`
+
+    return res.json(users)
+  } catch (error) {
+    console.error("Error is: ", error)
+    return res.status(500).json({ error: "Error when getting all users" })
+  }
+})
+
+server.keepAliveTimeout = 120 * 1000
+server.headersTimeout = 120 * 1000
