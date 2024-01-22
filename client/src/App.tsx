6a9f7559bc3d72929214bd63a236db1219c9b5e8
lazarevkristijan <lazarevkristijan@gmail.com>
2024-01-22 09:39:13 +0200

    add auth0 + basic login popup

--- client/src/App.tsx
+++ client/src/App.tsx
@@ -1,8 +1,11 @@
 import { useQuery } from "react-query"
 import { UserTypes } from "./Types"
 import { getAllUsers } from "./Utils/HeroUtils"
+import { useAuth0 } from "@auth0/auth0-react"
 
 const App = () => {
+  const { loginWithPopup } = useAuth0()
+
   const { data: allUsers, isLoading: areUsersLoading } = useQuery(
     "all-users",
     getAllUsers
@@ -15,6 +18,8 @@ const App = () => {
         : allUsers.map((user: UserTypes) => (
             <p key={user.id}>{user.first_name}</p>
           ))}
+
+      <button onClick={() => loginWithPopup()}>login</button>
     </>
   )
 }
