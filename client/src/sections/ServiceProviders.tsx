886d9d8302f1dd91554c482d3c5d6df59a1a874e
lazarevkristijan <lazarevkristijan@gmail.com>
2024-02-02 08:18:01 +0100

    show providers on /providers

--- client/src/sections/ServiceProviders.tsx
+++ client/src/sections/ServiceProviders.tsx
@@ -1,11 +1,22 @@
 import { useQuery } from "@tanstack/react-query"
 import { getAllServiceProviders } from "../Utils/ServiceProvidersUtils"
+import { ProviderTypes } from "../Types"
+import { getPfpLink } from "../Utils/SettingsUtils"
+import { useNavigate } from "react-router"
 
 const Providers = () => {
-  const { isLoading: areProvidersLoading, data: providers } = useQuery({
+  const navigate = useNavigate()
+
+  const { isLoading: areProvidersLoading, data: providers } = useQuery<
+    ProviderTypes[]
+  >({
     queryKey: ["providers"],
     queryFn: () => getAllServiceProviders(),
   })
+
+  console.log(providers)
+
+  if (!providers) return
 
   return (
     <div>
@@ -14,7 +25,44 @@ const Providers = () => {
       ) : (
         <>
           Всички служители:
-          <p>Количество: {providers.length}</p>
+          <div
+            style={{
+              display: "flex",
+              flexWrap: "wrap",
+              gap: 50,
+              marginTop: 20,
+            }}
+          >
+            {providers.map((provider: ProviderTypes) => (
+              <div
+                key={provider.id}
+                style={{
+                  border: "1px solid black",
+                  width: "fit-content",
+                  height: "fit-content",
+                  minWidth: 80,
+                  minHeight: 80,
+                  textAlign: "center",
+                  paddingTop: 10,
+                  cursor: "pointer",
+                }}
+                onClick={() => {
+                  navigate(`/служител/${provider.id}`)
+                }}
+              >
+                {provider.first_name} <br style={{ paddingBottom: 2 }} />
+                <img
+                  src={getPfpLink(provider.profile_picture)}
+                  alt={`${provider.first_name}'s profile picture`}
+                  style={{
+                    width: "50px",
+                    height: "50px",
+                    borderRadius: "50%",
+                  }}
+                />
+              </div>
+            ))}
+          </div>
         </>
       )}
     </div>
