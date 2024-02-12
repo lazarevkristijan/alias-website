e714f070ecf243821fcb2e3a2b9571419a1be69b
lazarevkristijan <lazarevkristijan@gmail.com>
2024-02-12 11:23:33 +0100

    basic hero styles

--- client/src/sections/Home.tsx
+++ client/src/sections/Home.tsx
@@ -1,12 +1,37 @@
+import "./Home.scss"
+
 const Home = () => {
   return (
-    <div
-      style={{
-        textAlign: "center",
-      }}
-    >
-      Алиас услуги
-    </div>
+    <section>
+      <section className="hero">
+        <div>
+          <h1>НАМЕРИ РЕШЕНИЕТО НА ТВОЯТ ПРОБЛЕМ ВЕДНАГА</h1>
+
+          <img
+            src="https://www.svgrepo.com/show/530572/accelerate.svg"
+            alt="Service image 1"
+            className="hero-img1"
+          />
+          <img
+            src="https://www.svgrepo.com/show/530572/accelerate.svg"
+            alt="Service image 2"
+            className="hero-img2"
+          />
+          <img
+            src="https://www.svgrepo.com/show/530572/accelerate.svg"
+            alt="Service image 3"
+            className="hero-img3"
+          />
+          <form onSubmit={(e) => e.preventDefault()}>
+            <input
+              type="text"
+              name="s"
+              placeholder="Услуга..."
+            />
+          </form>
+        </div>
+      </section>
+    </section>
   )
 }
 
