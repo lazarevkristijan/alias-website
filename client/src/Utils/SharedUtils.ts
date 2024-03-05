d580c2721e804a65ec196a1c35397a5aac8157bd
lazarevkristijan <lazarevkristijan@gmail.com>
2024-03-05 15:38:25 +0100

    move notification styles in css

--- client/src/Utils/SharedUtils.ts
+++ client/src/Utils/SharedUtils.ts
@@ -35,28 +35,11 @@ export const getAllServiceCategories = async () => {
 
 export const sendNotification = (message: string, success: boolean = false) => {
   const notification = document.createElement("div")
-
+  notification.classList.add("notification")
   notification.style.backgroundColor = success ? "#cfffdb" : "#ffb3b3"
-  notification.style.color = "#121212"
-  notification.style.padding = "10px"
-
-  notification.style.minWidth = "150px"
-  notification.style.minHeight = "75px"
-  notification.style.maxWidth = "250px"
-  notification.style.minHeight = "50px"
-  notification.style.width = "fit-content"
-  notification.style.height = "fit-content"
-
-  notification.style.textAlign = "center"
-  notification.style.borderRadius = "5px"
-
-  notification.style.position = "fixed"
-  notification.style.bottom = "80px"
-  notification.style.left = "30px"
-
+ 
   const textParagraph = document.createElement("p")
   textParagraph.textContent = message
-  textParagraph.style.margin = "0px"
 
   document.body.appendChild(notification)
   notification.appendChild(textParagraph)
